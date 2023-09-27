3332454d729b436d60909b03bd6a5a24fecc9e8e
mkhlshasryn <mukhlashasryono@gmail.com>
2023-09-27 09:30:46 +0700

    Jurnal01

---
+++ Jurnal.java
@@ -0,0 +1,10 @@
+public class Jurnal {
+
+    public static void main(String[] args) {
+
+        System.out.println("Nama : Mukhlash Asryono Rosi");
+        System.out.println("NIM : 607062300116");
+        System.out.println("Kelas : D3IF-47-04");
+        
+    }
+}
